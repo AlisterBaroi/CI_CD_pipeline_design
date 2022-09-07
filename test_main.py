9da97d8ed3179dae2e2b457f011fd7151523e65a
Alister Animesh Baroi <44337842+AlisterBaroi@users.noreply.github.com>
2022-09-07 19:13:20 +0800

    conditional tests

--- test_main.py
+++ test_main.py
@@ -5,4 +5,8 @@ class TestMain(unittest.TestCase):
     def test_str(self):
         result = main.code1()
         x = self.assertEqual(result, str)
-        print( 'Test Output: ', x)
+        if x != True:
+            print( 'Test Fail Output: ', x)
+        else:
+            print( 'Test Pass Output: ', x)
+
