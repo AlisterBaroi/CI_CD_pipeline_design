c9f7e2207318bc36ef86adafc292091e88ecc12d
Alister Animesh Baroi <44337842+AlisterBaroi@users.noreply.github.com>
2022-09-07 11:59:29 +0800

    fixed 1st unitTest

--- test_main.py
+++ test_main.py
@@ -3,7 +3,7 @@ import main
 
 class TestMain(unittest.TestCase):
     def test_str(self):
-        result main.code1()
+        result = main.code1()
         self.assertEqual(result, str)
 
 # type(text1) == int 
