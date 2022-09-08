19bcb782711ffbdfa3ca267836f3b0df202914f0
Alister Animesh Baroi <44337842+AlisterBaroi@users.noreply.github.com>
2022-09-08 16:10:00 +0800

    return

--- test_main.py
+++ test_main.py
@@ -4,6 +4,6 @@ import main
 class TestMain(unittest.TestCase):
     def test_str(self):
         result = main.code1()
-        return self.assertEqual(result, str)
+        self.assertEqual(result, str)
         
 
